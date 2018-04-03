d8ecaad9ca48ea7c7b45d627681ec54fd9a37c2b
Boina Babu <boina@Boinas-MacBook-Pro.local>
2018-04-03 10:52:46 +0200

    First commit

--- get-histo-price.py
+++ get-histo-price.py
@@ -1,5 +1,6 @@
 import time
 import requests
+import datetime
 from datetime import date
 from datetime import timedelta
 
@@ -12,19 +13,26 @@ choice = input("Type the ticker of your crypto:")
 currency='USD'
 
 date = input("Type the start date YYYY-MM-DD")
-before = input("Type the start date YYYY-MM-DD")
+date1 = input("Type the start date YYYY-MM-DD")
+
+#convert date to timestamp
+
 timestamp = time.mktime(time.strptime(str(date), '%Y-%m-%d'))
-timestamp1 = time.mktime(time.strptime(str(before), '%Y-%m-%d'))
+timestamp1 = time.mktime(time.strptime(str(date1), '%Y-%m-%d'))
+
+#one day is equal to 86400 in the timestamp language
+
 step = 86400
 x=timestamp
 
-getURL=priceURL+'?fsym='+choice+'&tsyms='+currency+'&ts='+str(round(timestamp,0))
-r = requests.get(getURL)
-print(r.json())
+#iterating through the range (timestamp,timestamp2) in order to print historical values
 
-numbers = range(int(timestamp),int(timestamp1),step)
+numbers = range(int(timestamp),int(timestamp1)+step,step)
 for count in numbers:
     x+=step
     getURL=priceURL+'?fsym='+choice+'&tsyms='+currency+'&ts='+str(x)
     r = requests.get(getURL)
+    date_format = datetime.datetime.fromtimestamp(int(x-step)).strftime('%Y-%m-%d')
+    print(date_format)
     print(r.json())
+    
